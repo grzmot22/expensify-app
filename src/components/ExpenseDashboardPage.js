b52492ce8d01c70904e20eb1f452b6425855787b
Grzmot22 <grzmot22@gmail.com>
2018-06-27 19:05:52 +0100

    Create AppRouter, ExpenseDashboardPage

---
+++ src/components/ExpenseDashboardPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
+
+const ExpenseDashboardPage = () => (
+    <div>
+    this is from my dashboard component
+    </div>
+
+);
+
+export default ExpenseDashboardPage;
---
+++ src/routers/AppRouter.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
+import ExpenseDashboardPage from './components/ExpenseDashboardPage';
+
+const AddExpensePage = () => (
+    <div>
+    this is from my add expense component
+    </div>
+
+);
+
+const EditExpensePage = () => (
+    <div>
+    this is from my edit expense component
+    </div>
+);
+
+const HelpPage = () => (
+    <div>
+    this is from my help expense component
+    </div>
+);
+
+const NotFoundPage = () => (
+    <div>
+    404! - <Link to="/">Go home</Link>
+    </div>
+);
+
+const Header = () => (
+    <header>
+    <h1>Expernsify</h1>
+    <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+    <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
+    <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
+    <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+    </header>
+);
+
+const AppRouter = () => (
+    <BrowserRouter>
+        <div>
+        <Header />
+        <Switch>
+        <Route path="/" component={ExpenseDashboardPage} exact={true}/>
+        <Route path="/create" component={AddExpensePage} />
+        <Route path="/edit" component={EditExpensePage} />
+        <Route path="/help" component={HelpPage} />
+        <Route  component={NotFoundPage} />
+        </Switch>
+        </div>
+    </BrowserRouter>
+);
+
+export default AppRouter;
