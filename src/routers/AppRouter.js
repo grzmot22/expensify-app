fa2e81e3bb6008cde115b285b9c73c6319e87ed2
Grzmot22 <grzmot22@gmail.com>
2018-06-27 19:14:48 +0100

    Create HelpPage

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -3,12 +3,7 @@ import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
 import ExpenseDashboardPage from './../components/ExpenseDashboardPage';
 import AddExpensePage from './../components/AddExpensePage';
 import EditExpensePage from './../components/EditExpensePage';
-
-const HelpPage = () => (
-    <div>
-    this is from my help expense component
-    </div>
-);
+import HelpPage from './../components/HelpPage';
 
 const NotFoundPage = () => (
     <div>
