2b523d443b96fb26980186863de00e4264f73de5
Grzmot22 <grzmot22@gmail.com>
2018-06-27 19:44:28 +0100

    Add ID to edit page

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const EditExpensePage = () => (
-    <div>
-    this is from my edit expense component
-    </div>
-);
+const EditExpensePage = (props) => {
+    console.log(props);
+    return (
+        <div>
+        Editing the exense with id of {props.match.params.id}
+        </div>
+    );
+
+};
 
 export default EditExpensePage;
