d7377f2ce44426558f862131f386cdff2c1e0d11
Grzmot22 <grzmot22@gmail.com>
2018-07-07 02:35:04 +0100

    redux-101: add if statement

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -2,8 +2,13 @@ import { createStore } from 'redux';
 
 const store = createStore((state={ count: 0 }) => {
     console.log('running');
-    return state;
-
+    if (action.type === 'INCREMENT') {
+       return {
+           count: state.count + 1
+       };
+    } else {
+        return state;
+    }
 });
 
 console.log(store.getState());
