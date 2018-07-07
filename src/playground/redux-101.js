96812cefaecf501bb4aa223dcb2371a92c24205f
Grzmot22 <grzmot22@gmail.com>
2018-07-07 02:29:20 +0100

    Redux-101: add 'INCREMENT'

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,9 +1,16 @@
 import { createStore } from 'redux';
 
 const store = createStore((state={ count: 0 }) => {
-
+    console.log('running');
     return state;
 
 });
+
+console.log(store.getState());
+
+store.dispatch({
+    type: 'INCREMENT'
+});
+
 
 console.log(store.getState());
