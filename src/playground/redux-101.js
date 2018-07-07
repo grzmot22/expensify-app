76896edbf16f36779bf4674a5655060045218fd3
Grzmot22 <grzmot22@gmail.com>
2018-07-07 02:36:15 +0100

    redux-101: fix error

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,7 +1,6 @@
 import { createStore } from 'redux';
 
-const store = createStore((state={ count: 0 }) => {
-    console.log('running');
+const store = createStore((state={ count: 0 , action}) => {
     if (action.type === 'INCREMENT') {
        return {
            count: state.count + 1
