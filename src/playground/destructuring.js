dfda8ba0356c2b87e681cb40907937f6b7acc59f
Grzmot22 <grzmot22@gmail.com>
2018-07-13 12:54:18 +0100

    destructuring: add person

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,1 +1,10 @@
-console.log('destructuring');
+const person = {
+    name: 'Andrew',
+    age: 23,
+    location: {
+        city: 'London',
+        temp: 22
+    }
+};
+
+console.log(`${person.name} is ${person.age}`);
