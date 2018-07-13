c712469048f4a55655ed981460462cb7246a9131
Grzmot22 <grzmot22@gmail.com>
2018-07-13 15:33:08 +0100

    redux-expensify: add expensesReducer

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,5 +1,22 @@
 import { createStore, combineReducers} from 'redux';
 
+// ADD_EXPENSE
+// REMOVE_EXPENSE
+// EDIT_EXPENSE
+// SET_TEXT-FILTER
+// SORT_BY_DATE
+// SORT_BY_AMOUNT  
+// SET_START_DATE
+// SET_END_DATE
+
+// Expenses Reducers
+
+const expensesReducerDefaultState = [];
+
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+
+};
+
 const demoState = {
     expenses: [{
         id: 'sdfsdffsd',
