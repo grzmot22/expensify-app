324deacc0333b4a685fc1b76ac36075f7dbe7cb3
Grzmot22 <grzmot22@gmail.com>
2018-07-13 13:45:07 +0100

    dectructuring: add item

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,37 +1,37 @@
 // 
 // Obeject dectrucuring
 // 
-const person = {
-    name: 'Andrew',
-    age: 23,
-    location: {
-        city: 'London',
-        temp: 22
-    }
-};
+// const person = {
+//     name: 'Andrew',
+//     age: 23,
+//     location: {
+//         city: 'London',
+//         temp: 22
+//     }
+// };
 
-const {name: firstName = 'Anonymouns', age} = person;
-// const name = person.name;
-// const age = person.age;
+// const {name: firstName = 'Anonymouns', age} = person;
+// // const name = person.name;
+// // const age = person.age;
 
-console.log(`${firstName} is ${age}`);
+// console.log(`${firstName} is ${age}`);
 
-const {city, temp: temperature} =person.location;
-if (city && temperature) {
-    console.log(`It's ${temperature} in ${city}`)
-}
+// const {city, temp: temperature} =person.location;
+// if (city && temperature) {
+//     console.log(`It's ${temperature} in ${city}`)
+// }
 
-const book = {
-    title: 'Ego is the Enemy',
-    author: 'Ryan Holiday',
-    publisher: {
-        name: 'Penguin'
-    }
-};
+// const book = {
+//     title: 'Ego is the Enemy',
+//     author: 'Ryan Holiday',
+//     publisher: {
+//         name: 'Penguin'
+//     }
+// };
 
-const {name: publisherName = 'Self-Published'} = book.publisher;
+// const {name: publisherName = 'Self-Published'} = book.publisher;
 
-console.log(publisherName);
+// console.log(publisherName);
 
 // 
 // Array destucturing
@@ -39,6 +39,12 @@ console.log(publisherName);
 
 const address = ['99 Cambridge Street','Bedford' ,'Bedfordshire', 'MK40 3EE']
 
-const[street, city, state, zip] = address;
+const [street, city, state = 'New York', zip] = address;
 
 console.log(`You are in ${city} ${state}.`);
+
+const item = ['Coffee (hot)', '$2.00', '$2.50','$2.75'];
+
+const [itemName, ,mediumSize,] = item;
+
+console.log(`A ${itemName} costs ${mediumSize}`);
