ef24e9b4799e0dd5a5a7574673a75025a90e2fcd
Grzmot22 <grzmot22@gmail.com>
2018-07-18 04:03:29 +0100

    Create ExpenseForm

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
+import ExpenseForm from './ExpenseForm';
 
 const AddExpensePage = () => (
     <div>
-    this is from my add expense component
+    <h1>Add Expense</h1>
+    <ExpenseForm />
     </div>
 
 );
