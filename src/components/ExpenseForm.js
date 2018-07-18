63a3ba55c7af3eb24c6f55591b2dda8988bcd735
Grzmot22 <grzmot22@gmail.com>
2018-07-18 14:57:44 +0100

    ExpenseForm: Let's submit button passing data

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -41,7 +41,12 @@ export default class ExpenseForm extends React.Component {
             this.setState(() =>({error: 'Please provide decription and amount.'}))
         } else {
             this.setState(() =>({error: ''}))
-            console.log('submitted!');
+            this.props.onSubmit({
+                description: this.state.description,
+                amount: parseFloat(this.state.amount, 10) * 100,
+                createdAt: this.state.createdAt.valueOf();,
+                note: this.state.note
+            });
         }
     }
     render() {
