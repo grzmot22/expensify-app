a330b734ab0836cb552518a4b2167e209552686d
Grzmot22 <grzmot22@gmail.com>
2018-10-03 18:46:52 +0100

    expenses.test.js: Test edit expense

--- src/tests/expenses.test.js
+++ src/tests/expenses.test.js
@@ -1,9 +1,21 @@
 import {addExpense, editExpense, removeExpense } from '../actions/expenses';
 
-Test('should setup remove expense action object', () => {
+test('should setup remove expense action object', () => {
     const action = removeExpense({ id: '123abc' });
     expect(action).toEqual({
         type: 'REMOVE_EXPENSE',
         id: '123abc'
+    });
+});
+
+test('should setup edit expense action object', () => {
+    const action = editExpense( '123abc', {note: 'New note value'});
+    expect(action).toEqual({
+        type: 'EDIT_EXPENSE',
+        id: '123abc',
+        updates: {
+            note: 'New note value'
+        }
+        
     });
 });
