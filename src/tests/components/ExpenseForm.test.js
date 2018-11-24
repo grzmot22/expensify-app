d6efca54fbc29486802fc4af921fa4090a10fb41
Grzmot22 <grzmot22@gmail.com>
2018-11-24 02:06:36 +0000

    ExpenseForm.test.js: add test should set description on input change

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import ExpenseForm from '../../components/ExpenseForm';
 import expenses from '../fixtures/expenses';
+import { wrap } from 'module';
 
 test('should render ExpenseForm correctly', () => {
   const wrapper = shallow(<ExpenseForm />);
@@ -20,4 +21,13 @@ test('should render error for invalid form submission', () => {
     });
   expect(wrapper.state('error').length).toBeGreaterThan(0);
   expect(wrapper).toMatchSnapshot();
+})
+
+test('should set description on input change', () => { 
+  const wrapper = shallow(<ExpenseForm />);
+  const value = 'New description';
+  wrapper.find('input').at(0).simulate('change', {
+      target: { value }
+  });
+  expect(wrapper.state('description')).toBe(value);
 });
