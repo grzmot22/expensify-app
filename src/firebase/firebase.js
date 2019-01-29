a184a5241c9f7e4a4db394cb52f354c2923453bc
Grzmot22 <grzmot22@gmail.com>
2019-01-29 17:30:59 +0000

    Install firebase module

---
+++ src/firebase/firebase.js
@@ -0,0 +1,24 @@
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyBRAw1OqD10OFvUT6riId6Iyai4XTjtAGU",
+    authDomain: "expensify-a983a.firebaseapp.com",
+    databaseURL: "https://expensify-a983a.firebaseio.com",
+    projectId: "expensify-a983a",
+    storageBucket: "expensify-a983a.appspot.com",
+    messagingSenderId: "600470454014"
+};
+
+firebase.initializeApp(config);
+
+const database = firebase.database();
+
+database.ref().set({
+    name: 'Bob S',
+    age: 23,
+    isSingle: true,
+    location: {
+        city: 'London',
+        country: 'United Kingdom'
+    }
+});
