80a8eab39113735eefeae7bc570f42fe48cf6979
Grzmot22 <grzmot22@gmail.com>
2019-02-07 02:06:43 +0000

    More play with firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -13,30 +13,69 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-database.ref().set({
-    name: 'Bob S',
-    age: 23,
-    stressLevel: 5,
-    job: {
-        title: 'Sotfware developer',
-        company: 'Google'
-    },
-    location: {
-        city: 'London',
-        country: 'United Kingdom'
-    }
-}).then(() => {
-    console.log('Data is saved');
-}).catch((e) => {
-    console.log('This failed.', e);
+// Setup data sub -> bob is a Software dev at amazon.
+// change the data and make it reprints 
+
+database.ref().on('value', (snapshot) => {
+    const val = snapshot.val();
+    console.log(`${val.name} is a ${val.job.title} at ${val.job.company}.`);
+}, (e) => {
+        console.log('Error with data fetching', e);
 });
 
-database.ref().update({
-    stressLevel: 9,
-    'job/company': 'Amazon',
-    'location/city': 'Seattle'
+// const onValueChange = database.ref().on('value', (snapshot) => {
+//     console.log(snapshot.val());
+// }, (e) => {
+//     console.log('Error with data fetching', e);
+// });
+
+// setTimeout(() => {
+//     database.ref('age').set(25);
+// }, 2300);
+
+// setTimeout(() => {
+//     database.ref().off(onValueChange);
+// }, 7300);
+
+// setTimeout(() => {
+//     database.ref('age').set(30);
+// }, 10400);
+
+
+// database.ref('location/city')
+// .once('value')
+// .then((snapshot) => {
+//     const val = snapshot.val();
+//     console.log(val);
+// })
+// .catch((e) => {
+//     console.log('Error fetching data', e);
+// });
+
+// database.ref().set({
+//     name: 'Bob S',
+//     age: 23,
+//     stressLevel: 5,
+//     job: {
+//         title: 'Sotfware developer',
+//         company: 'Google'
+//     },
+//     location: {
+//         city: 'London',
+//         country: 'United Kingdom'
+//     }
+// }).then(() => {
+//     console.log('Data is saved');
+// }).catch((e) => {
+//     console.log('This failed.', e);
+// });
+
+// database.ref().update({
+//     stressLevel: 9,
+//     'job/company': 'Amazon',
+//     'location/city': 'Seattle'
     
-});
+// });
 
 // database.ref('isSingle')
 // .remove()
