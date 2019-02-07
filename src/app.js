3625fcdc0f227fac296b2ce10ec041130532e1f2
Grzmot22 <grzmot22@gmail.com>
2019-02-07 15:19:30 +0000

    Remove unused lines

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ import 'react-dates/lib/css/_datepicker.css';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import './firebase/firebase';
-import prommises from './playground/promises';
+
 
 const store = configureStore();
 
@@ -19,9 +19,9 @@ store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000 }));
 store.dispatch(addExpense({ description: 'Rent', amount: 109500} ));
 
 const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 
-console.log(visibleExpenses);
+// console.log(visibleExpenses);
 
 const jsx = (
     <Provider store={store}>
