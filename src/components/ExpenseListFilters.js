08bb7a4bf4611e5f26ff0200ed71347e4b3dcf12
Grzmot22 <grzmot22@gmail.com>
2019-02-10 09:46:55 +0000

    Fix some display problems

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -30,43 +30,41 @@ export class ExpenseListFilters extends React.Component {
     };
     render() {
         return (
-            <div className="content-container">
+        <div className="content-container">
                 <div className="input-group">
-                    <div className="input=group__item">
-                        <input 
-                            type="text" 
-                            className="text-input"
-                            placeholder="Search expenses"
-                            value={this.props.filters.text} 
-                            onChange={this.onTextChange}
-                            />
+                    <div className="input-group__item">
+                        <input
+                        type="text"
+                        className="text-input"
+                        placeholder="Search expenses"
+                        value={this.props.filters.text}
+                        onChange={this.onTextChange}
+                        />
                     </div>
-                    <div className="input=group__item">
-                    <select
+                    <div className="input-group__item">
+                        <select
                         className="select"
                         value={this.props.filters.sortBy}
                         onChange={this.onSortChange}
-                    >
+                        >
                         <option value="date">Date</option>
                         <option value="amount">Amount</option>
-                    </select>
+                        </select>
                     </div>
-                    <div className="input=group__item">
-                    <DateRangePicker 
+                    <div className="input-group__item">
+                        <DateRangePicker
                         startDate={this.props.filters.startDate}
-                        startDateId={this.state.startDateId}
                         endDate={this.props.filters.endDate}
-                        endDateId={this.state.endDateId}
                         onDatesChange={this.onDatesChange}
                         focusedInput={this.state.calendarFocused}
                         onFocusChange={this.onFocusChange}
                         showClearDates={true}
                         numberOfMonths={1}
                         isOutsideRange={() => false}
-                    />
+                        />
                     </div>
-                </div>       
-        </div>
+                </div>
+            </div>
         );
     }
 };
